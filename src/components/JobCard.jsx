ea854b120bf1e33482c382a0486545af867ae3fc
Mauricio Bianchi <mobianchi@cloodie.com>
2020-11-11 16:31:15 -0300

    fix delete jobcard

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -57,11 +57,11 @@ export default class JobCard extends React.Component{
     render(){
         return (
             <Card className="home_card">
-            <Button onClick={ this.eliminarTrabajo} >
-                                  <svg   style={{color:this.state.color }}   width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                  <path  onMouseOver={this.cambiarColor} onMouseOut={this.cambiarColor} fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+            { !this.state.guestMode ? <Button onClick={ this.eliminarTrabajo} >
+                                  <svg   style={{color:this.state.color }}   width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-x-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                  <path  onMouseOver={this.cambiarColor} onMouseOut={this.cambiarColor} fillRule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
                                 </svg>
-                                </Button>
+        </Button> : <div></div> }
               <Card.Img variant="top" src={this.getRandomImage()} />
               {
                 !this.state.guestMode ? <FontAwesomeIcon onClick={this.openEditModal} className="icon-edit" icon={faPen} size="lg"> </FontAwesomeIcon> : <div></div>
